646e407d70d9a66fa3b090da6de98cff3522168b
DriftingShade <sarus333@yahoo.com>
2024-09-01 16:13:44 -0600

    again

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from flask_app import app
+from flask_app.controllers import users
+from flask_app.controllers import pizzas
+from flask_app.controllers import orders
 
 if __name__ == "__main__":
     app.run()
