b41a1320cf73923e3bbe0307f15f7a4353778f6f
DriftingShade <sarus333@yahoo.com>
2024-09-01 18:02:07 -0600

    I hate life

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-# from flask_app import app
-# from flask_app.controllers import users
-# from flask_app.controllers import pizzas
-# from flask_app.controllers import orders
+from flask_app import app
+from flask_app.controllers import users
+from flask_app.controllers import pizzas
+from flask_app.controllers import orders
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
